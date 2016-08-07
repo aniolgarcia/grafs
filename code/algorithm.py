95e6b9ce06c0cd3ccb5f75135a6b722e92eaa056
Aniol Garcia <aniolgarcia@gmail.com>
2016-08-07 20:22:05 +0200

    Actualització 7_8

--- code/algorithm.py
+++ code/algorithm.py
@@ -1,4 +1,5 @@
 parent={}
+topo=[] 
 def DFS(Adj):
     node=[]
     for i in range(0, len(Adj)):
@@ -10,6 +11,9 @@ def DFS(Adj):
             print s            
             parent[s]=None
             DFS_recursive(Adj, s)
+    print "Recursion order (topological sort for directed acyclic graphs):"
+    topo.reverse()    
+    print topo
 
 
 def DFS_recursive(Adj, s):
@@ -18,6 +22,8 @@ def DFS_recursive(Adj, s):
             print v
             parent[v]=s
             DFS_recursive(Adj, v)
+    topo.append(s)
+        
 
 def BFS(Adj, s):
     level={s:0}
@@ -38,7 +44,30 @@ def BFS(Adj, s):
         i+=1
     print level
     
+def Dijkstra(Adj, s):
+    Q={}
+    dist={}
+    for i in range(0, len(Adj)):
+        Q[i]=float("inf")
+        dist[i]=float("inf")
+    Q[s]=0
+    while Q:
+        u = min(Q, key=Q.get)
+        dist[u] = Q[u]
+        for v in Adj[u]:
+            if v in Q:
+                if Q[v] > Q[u] + Adj[u][v]:
+                    Q[v] = Q[u] + Adj[u][v]
+        Q.pop(u)
+    print dist
 
 v=[[1,2],[0,2,4],[0,1,3],[2,4,5],[1,3,5],[3,4]]
 w=[[1,2],[],[],[0,1,2]]
-DFS(w)
+dag=[[1],[2,3,5],[4],[7],[],[6],[],[]]
+weight=[{1:10,2:3},{2:1, 3:2},{1:4, 3:8, 4:2},{4:7},{3:9}]
+killer=[{1:16,2:0},{2:-32},{3:8,4:0},{4:-16},{5:4,6:0},{6:-8},{7:2,8:0},{8:-4},{9:1,10:-2},{}]
+non_co=[{1:1, 2:1},{2:1, 3:1},{3:1, 5:1},{4:1},{},{},{},{6:1}]
+non_dir=[{1:8,3:2},{0:8,2:5,3:3,4:1},{1:5,3:2,4:4},{0:2,1:3,2:2},{1:1,2:4}]
+Dijkstra(non_dir,0)
+
+
